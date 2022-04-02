cf20f6e5e205f73f58e3a1dd716a66533b9167bb
Fernando Ortega <fernando.ortega@nuvu.cc>
2022-04-02 17:17:17 -0500

    Ahora si el commit inicial

---
+++ savingFaces.py
@@ -0,0 +1,41 @@
+import cv2
+import os
+
+facesFolder = 'faces'
+
+if not os.path.exists(facesFolder):
+    print('Carpeta creada: '+facesFolder)
+    os.makedirs(facesFolder)
+
+cap = cv2.VideoCapture(1)
+
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+
+count = 0
+while True:
+    ret,frame = cap.read()
+    print(frame)
+    frame = cv2.flip(frame,1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    auxFrame = frame.copy()
+
+    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+
+    k = cv2.waitKey(1)
+    print (k)
+    if k == 27:
+        break
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y),(x+w,y+h),(128,0,255),2)
+        rostro = auxFrame[y:y+h,x:x+w]
+        rostro = cv2.resize(rostro,(150,150), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(facesFolder+'/rostro_{}.jpg'.format(count),rostro)
+        cv2.imshow('rostro',rostro)
+        count = count +1
+    cv2.rectangle(frame,(10,5),(450,25),(255,255,255),-1)
+    cv2.putText(frame,'Presione s, para almacenar los rostros encontrados',(10,20), 2, 0.5,(128,0,255),1,cv2.LINE_AA)
+    cv2.imshow('frame',frame)
+
+cap.release()
+cv2.destroyAllWindows()
